0cd94aec59bac4132e4f6d2b30591a66c1ce9cb4
Martin Pfundmair <dev@martin.pfundmair.de>
2024-02-08 08:31:41 +0900

    Initial Commit

---
+++ DebugIntentPackage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DebugIntentPackage
+//
+//  Created by Martin Pfundmair on 2024-02-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DebugIntentPackage/DebugIntentPackageApp.swift
@@ -0,0 +1,17 @@
+//
+//  DebugIntentPackageApp.swift
+//  DebugIntentPackage
+//
+//  Created by Martin Pfundmair on 2024-02-08.
+//
+
+import SwiftUI
+
+@main
+struct DebugIntentPackageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
